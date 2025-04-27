9479992c05dc3e6aaaadf43164b40ad3e3c5273e
Sherra Paltingca <sherrajayetuble@gmail.com>
2025-04-27 16:30:59 -0400

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mybookshelf/',
+  base: '/booktracker/',
   plugins: [
     react(),
     tailwindcss()
